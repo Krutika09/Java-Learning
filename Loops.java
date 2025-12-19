fc8cdf597fe4d43d76bbb1d9927ab66aeca1f6ab
Krutika Yawale <148604628+Krutika09@users.noreply.github.com>
2025-12-19 08:37:53 +0530

    Add examples of loop structures in Java

---
+++ Loops.java
@@ -0,0 +1,31 @@
+// ===========For Loop===========
+// public class ForLoop{
+//     public static void main(String args[]) {
+//         for(int a=3; a<=5; a++) {
+//         System.out.println(a); }
+//     }
+// }
+
+// ===========While Loop===========
+// public class WhileLoop{
+//     public static void main(String args[]) {
+//         int b = 1;
+//         while(b <= 10){
+//         System.out.println("While loop :" + b);
+//             // System.out.println(b);
+//             b++;
+//         }
+//     }
+// }
+
+// ===========Do While Loop===========
+// public class DoWhileLoop{
+//     public static void main(String args[]){
+//         int c = 7; 
+//         do {
+//             System.out.println(c);
+//             c++;
+//         // } while (c <= 5);
+//         } while(c < = 10);
+//     }
+// }
