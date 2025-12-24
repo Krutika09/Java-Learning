9ccbe4bb9a4145db256d2a6099f8dd7eb05f6780
Krutika Yawale <148604628+Krutika09@users.noreply.github.com>
2025-12-24 18:23:44 +0530

    Add comments on method functionality
    Added comments explaining the purpose of methods.

--- Method.java
+++ Method.java
@@ -1,3 +1,6 @@
+// A method is a block of code that performs a specific task.
+// Dividing a complex problem into smaller chunks makes your program easy to understand and reusable.
+
 public class Demo {
 
     public static void main(String[] args) {
