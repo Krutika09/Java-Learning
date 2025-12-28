fcc93d58561d296f4f93f387206bb60376768e7d
Krutika Yawale <148604628+Krutika09@users.noreply.github.com>
2025-12-28 18:19:55 +0530

    Add Student class with read method and main method

---
+++ Class-Object-Method.java
@@ -0,0 +1,19 @@
+// This is a CLASS
+class Student {
+
+    // This is a METHOD
+    void read() {
+        System.out.println("Student is reading");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        // This is an OBJECT of Student class
+        Student s1 = new Student();
+
+        // Calling METHOD using OBJECT
+        s1.read();
+    }
+}
