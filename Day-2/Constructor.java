28ded09ac2fcb2987a5a1b44bc727f30417bcc2b
Krutika Yawale <148604628+Krutika09@users.noreply.github.com>
2026-01-01 18:30:37 +0530

    Implement Constructor examples in Constructor.java
    Added examples of constructors in Java with explanations.

---
+++ Day-2/Constructor.java
@@ -0,0 +1,33 @@
+// A constructor is a special method in Java that is called automatically when an object is created.
+class CreatingConstructor{ 
+// Class name and constructor name must be same
+    String name;  // Initializaing instance variable
+    
+    CreatingConstructor(){ // Constructor
+        name = "Krutika";
+    }
+    
+    public static void main(String[] args){
+        // constructor called
+        CreatingConstructor obj = new CreatingConstructor(); 
+        System.out.println("Constructor is create by: " + obj.name);
+    }
+}
+
+
+class Student {
+
+    Student() {   // constructor
+        System.out.println("Constructor called");
+    }
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();  // new keyword
+    }
+}
+// 1. Student s1 = new Student();
+// - यहाँ new keyword use हुआ
+// - इससे Student का object बनता है
+// 2. जैसे ही object बना
+// - Constructor अपने आप call हो गया
